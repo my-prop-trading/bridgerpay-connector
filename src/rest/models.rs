6980724a72be91a034c97dcc0242ee48089b37f0
Maxim Gorin <onlinexmax@gmail.com>
2025-02-17 14:34:04 +0300

    Add model

---
+++ src/rest/models.rs
@@ -0,0 +1,7 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LoginRequest {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LoginResponse {}
