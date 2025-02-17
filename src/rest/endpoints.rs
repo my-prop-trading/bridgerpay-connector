629b239e54d4c9df5ae9d402f92c9dc65acb5def
Maxim Gorin <onlinexmax@gmail.com>
2025-02-17 14:38:40 +0300

    Impl login endpoint

--- src/rest/endpoints.rs
+++ src/rest/endpoints.rs
@@ -7,12 +7,12 @@ pub enum RestApiEndpoint {
 
 impl From<&RestApiEndpoint> for String {
     fn from(item: &RestApiEndpoint) -> Self {
-        let api_version = "v1";
+        let api_version = "v2";
         let api_name = "rest-api";
 
         match item {
             RestApiEndpoint::Login => {
-                format!("/{api_name}/{}/users/create", api_version)
+                format!("/{api_version}/auth/login", )
             }
         }
     }
