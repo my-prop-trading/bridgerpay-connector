43bca4b926fba399f55d1682608f18cc0bffa17e
Maxim Gorin <onlinexmax@gmail.com>
2025-02-17 14:37:59 +0300

    Impl login model

--- src/rest/models.rs
+++ src/rest/models.rs
@@ -1,7 +1,32 @@
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct LoginRequest {}
+pub struct LoginRequest {
+    pub user_name: String,
+    pub password: String,
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct LoginResponse {}
+pub struct LoginResponse {
+    pub response: ResponseModel,
+    pub result: LoginResultModel,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LoginResultModel {
+    pub refresh_token: String,
+    pub access_token: AccessTokenModel,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AccessTokenModel {
+    pub token: String,
+    pub expires_in: i64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResponseModel {
+    pub status: String,
+    pub code: i32,
+    pub message: String,
+}
