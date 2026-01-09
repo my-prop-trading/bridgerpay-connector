e7efd51b980580dd735d4944a4066821226d0f91
MichaelRedkinFintechb2b <m.redkin@fintechb2b.com>
2026-01-09 13:35:25 +0200

    refactor: remove old mapping.

--- src/webhook.rs
+++ src/webhook.rs
@@ -74,10 +74,6 @@ pub struct Charge {
     pub mid_type: Option<String>,
     pub cft_id: Option<String>,
     pub order_id: Option<String>,
-    #[serde(default)]
-    pub decline_reason: Option<String>,
-    #[serde(default)]
-    pub decline_code: Option<String>,
 }
 
 #[derive(strum::Display, Debug, Clone, Serialize, Deserialize, PartialOrd, PartialEq)]
